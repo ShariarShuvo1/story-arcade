cdcfd210fdc6e19115b45e0237ff4a37f456fed4
ShariarShuvo1 <shariaislamshuvo@gmail.com>
2024-06-14 21:50:12 +0600

    Frontend Setup

--- Frontend/storyarcade/src/App.js
+++ Frontend/storyarcade/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
+import RoutingPage from "./components/RoutingPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RoutingPage />,
+		children: [
+			{ path: "/", element: <Homepage /> },
+			{ path: "/home", element: <Homepage /> },
+		],
+	},
+]);
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ Frontend/storyarcade/src/components/RoutingPage.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+function RoutingPage() {
+	return (
+		<>
+			<Outlet />
+		</>
+	);
+}
+
+export default RoutingPage;
