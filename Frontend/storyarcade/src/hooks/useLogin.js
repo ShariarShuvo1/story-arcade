14acb0a3d5c847b2981dae3b304cf881acde5d15
Lamia-Saiyara <lamiasaiyara7@gmail.com>
2024-07-04 20:05:00 +0600

    I have added Login functionality

--- Frontend/storyarcade/src/hooks/useLogin.js
+++ Frontend/storyarcade/src/hooks/useLogin.js
@@ -1,34 +1,41 @@
-import { useState } from 'react';
-import { useAuthContext } from './useAuthContext';
-import { loginUser } from '../api/usersAPI';
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { loginUser } from "../api/usersAPI";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(null);
-    const { dispatch } = useAuthContext();
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(null);
+	const { dispatch } = useAuthContext();
+	const navigate = useNavigate();
 
-    const login = async (email, password) => {
-        setIsLoading(true);
-        setError(null);
+	const login = async (email, password) => {
+		setIsLoading(true);
+		setError(null);
 
-        try {
-            const response = await loginUser(email, password);
-            const json = await response.json()
+		try {
+			const response = await loginUser(email, password);
 
-            if (response.status === 200) {
-                localStorage.setItem('jwt', JSON.stringify(json));
-                dispatch({ type: 'LOGIN', payload: json });
-            } else if (response.status === 400) {
-                setError(response.data.message);
-            } else if (response.status === 401) {
-                setError(response.data.message);
-            }
-            setIsLoading(false);
-        } catch (err) {
-            setIsLoading(false);
-            setError('Network Error');
-        }
-    };
+			if (response.status === 201) {
+				const jwt_token = response.data.token;
+				const verified = response.data.verified;
+				localStorage.setItem("jwt", JSON.stringify(jwt_token));
+				if (verified) {
+					dispatch({ type: "LOGIN", payload: jwt_token });
+				} else {
+					navigate("/emailVerify");
+				}
+			} else if (response.status === 400) {
+				setError(response.data.message);
+			} else if (response.status === 401) {
+				setError(response.data.message);
+			}
+			setIsLoading(false);
+		} catch (err) {
+			setIsLoading(false);
+			setError("Network Error");
+		}
+	};
 
-    return { login, isLoading, error };
+	return { login, isLoading, error };
 };
