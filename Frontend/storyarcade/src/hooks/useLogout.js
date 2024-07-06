3e55d40cfff2acc27b65642514fa6e1fa875dd8f
ShariarShuvo1 <shariaislamshuvo@gmail.com>
2024-07-07 02:52:08 +0600

    Project Cleanup

--- Frontend/storyarcade/src/hooks/useLogout.js
+++ Frontend/storyarcade/src/hooks/useLogout.js
@@ -1,13 +1,13 @@
-import { useAuthContext } from './useAuthContext'
+import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
-    const { dispatch } = useAuthContext()
+	const { dispatch } = useAuthContext();
 
-    const logout = () => {
-        localStorage.removeItem('jwt')
+	const logout = () => {
+		localStorage.removeItem("jwt");
 
-        dispatch({ type: 'LOGOUT' })
-    }
+		dispatch({ type: "LOGOUT" });
+	};
 
-    return { logout }
-}
+	return { logout };
+};
