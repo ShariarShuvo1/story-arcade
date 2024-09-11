4faeab7a0924e67ae5b35ac438b657f308e831b1
ShariarShuvo1 <shariaislamshuvo@gmail.com>
2024-09-11 22:59:39 +0600

    add initial filter to popular for homepage

--- Frontend/storyarcade/src/components/Homepage/Footer.js
+++ Frontend/storyarcade/src/components/Homepage/Footer.js
@@ -2,8 +2,9 @@ import { useNavigate } from "react-router-dom";
 function Footer() {
 	const nav = useNavigate();
 	return (
-		<div className="w-full bg-gray-700 rounded-t-md px-2 flex items-center text-white h-8 fixed bottom-0">
+		<div className="w-full bg-gray-700 rounded-t-md px-2 flex gap-4 items-center text-white h-8 fixed bottom-0">
 			<button onClick={() => nav("/support")}>Support</button>
+			<button onClick={() => nav("/dashboard")}>Dashboard</button>
 		</div>
 	);
 }
